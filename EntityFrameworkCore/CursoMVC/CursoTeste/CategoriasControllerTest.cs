3fd44d195cad023dd65dfa582d2572cadb4abfad
Gabriel Serafim Souza <gabriel.souza@b2egroup.com.br>
2020-08-22 19:02:23 -0300

    first commit

---
+++ EntityFrameworkCore/CursoMVC/CursoTeste/CategoriasControllerTest.cs
@@ -0,0 +1,20 @@
+﻿using CursoMVC.Models;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoTeste
+{
+    class CategoriasControllerTest
+    {
+        private readonly Mock<DbSet<Categoria>> _mokSet;
+        private readonly Mock<DbSet<Context>> _mokContext;
+        private readonly Categoria _mokCategoria;
+        public  CategoriasControllerTest ()
+            { 
+
+            }
+    }
+}
